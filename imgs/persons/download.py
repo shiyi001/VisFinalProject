1dfec4c852c7047b2677bd391a72511b4d12638f
ZhengLiangfeng <zhengliangfeng@sensetime.com>
2019-01-22 21:03:39 +0800

    add more pics

--- imgs/persons/download.py
+++ imgs/persons/download.py
@@ -6,5 +6,8 @@ data = json.load(open("pic.json", "r"))
 for nobles in data:
     for i in range(1, len(nobles)):
         if (nobles[i][6].startswith("http")):
-            print (nobles[i][6])
-            os.system("wget {}".format(nobles[i][6]))
+            if os.path.exists(nobles[i][6].split("/")[6]):
+                continue
+            else:
+                print (nobles[i][6])
+                os.system("wget {}".format(nobles[i][6]))
--- readme.md
+++ readme.md
@@ -1,4 +1,2 @@
 # Vis Final Project
 这是数据可视化的大作业--诺贝尔奖可视化，由[shiyi001](https://github.com/shiyi001),[dhx000](https://github.com/dhx000),[zhao1liang](https://github.com/zhao1iang)协作完成。
-
-## 
